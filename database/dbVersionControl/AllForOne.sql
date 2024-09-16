b6c5b443985482d99e2d61496ee1fdd833a71608
Samir Sai <94905339+samirsai0@users.noreply.github.com>
2024-09-16 17:32:42 +0545

    Structure for storing questions

---
+++ database/dbVersionControl/AllForOne.sql
@@ -0,0 +1,154 @@
+CREATE DATABASE `grade_6`;
+CREATE TABLE `grade_6`.`english` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_6`.`health` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_6`.`math` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_6`.`science` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE DATABASE `grade_7`;
+CREATE TABLE `grade_7`.`english` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_7`.`health` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_7`.`math` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_7`.`science` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE DATABASE `grade_8`;
+CREATE TABLE `grade_6`.`english` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_8`.`health` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_8`.`math` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_8`.`science` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE DATABASE `grade_9`;
+CREATE TABLE `grade_9`.`english` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_9`.`health` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_9`.`math` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `grade_9`.`science` (
+  `Question_ID` int(16) NOT NULL,
+  `Question_Title` text NOT NULL,
+  `Option_1` varchar(255) NOT NULL,
+  `Option_2` varchar(255) NOT NULL,
+  `Option_3` varchar(255) NOT NULL,
+  `Option_4` varchar(255) NOT NULL,
+  `Correct_Option` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
