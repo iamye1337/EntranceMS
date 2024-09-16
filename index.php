506aed8ff153d561fb2b080fc4c38bc47e27c22f
samirsai0 <shahisameer54@gmail.com>
2024-09-16 22:13:55 +0545

    Setup redirection depending on wether user is logged in or not

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-
-header("location:student/login.html");
+include "database/connectdb.php";
+include "session_handler.php";
+if (isLoggedIn()) {
+    header("Location:student/profile.php");
+} else {
+    header("Location:student/login.php");
+}
+?>
