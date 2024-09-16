6d2c35aab777157e88d5af46f5aaefc1042135c9
samirsai0 <shahisameer54@gmail.com>
2024-09-16 08:58:49 +0545

    init

--- database/connectdb.php
+++ database/connectdb.php
@@ -11,4 +11,4 @@ $conn = new mysqli(hostname: $servername, username: $username, password: $passwo
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "shit";
+?>
