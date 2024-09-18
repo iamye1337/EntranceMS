7b2f5efbb1fe03cc0693753da6d5ec31a7370671
Joseph Maharjan <89244085+Josephmaharjan@users.noreply.github.com>
2024-09-18 13:00:32 +0545

    Update submitted.php in local

--- exampage/submitted.php
+++ exampage/submitted.php
@@ -13,13 +13,13 @@
 <body>
     <!-- title bar -->
     <div class="d-flex p-2 justify-content-center align-items-center" style="background-color: #002561;">
-        <h1 class="fs-3 fw-normal text-light">Adarsha Saula Yubak Secondary School</h1>
+        <p class="fs-1 fw-medium text-light">Adarsha Saula Yubak Secondary School</p>
     </div>
 
     <!-- action bar -->
     <div class="d-flex justify-content-between p-2" style="background-color: white; margin-bottom: calc(6vh);">
         <!-- logo -->
-        <img src="../images/adarshaLogo.png" style="height: 40px;" alt="">
+        <img src="../images/adarshaLogo.png" style="height: 70px;" alt="">
         <!-- question number -->
 
     </div>
@@ -28,18 +28,19 @@
     <div class="container d-flex justify-content-center align-items-center">
         <!----------------------- main Container -------------------------->
         <div class="border rounded-5 p-5 bg-white shadow box-area text-center">
-            <img class="img-fluid w-auto" style="max-width: 250px;" src="../images/greenCheckmark.png" alt="">
-            <p class="text-center p-6 fs-4">
+            <img class="img-fluid w-auto" style="max-width: 300px;" src="../images/greenCheckmark.png" alt="">
+            <p class="text-center p-5 fs-1">
                 Your response has been recorded!
             </p>
             <a href="../student/profile.php">
-                <div class="d-grid rounded">
-                    <button class="btn btn-primary btn-lg fs-6">Go back to your Profile</button>
+                    <div class="d-grid rounded" >
+                    <button class="btn btn-primary btn-lg fs-4">Go back to your Profile</button>
                 </div>
             </a>
         </div>
+
     </div>
-    <script src="../js/logout.js"></script>
+<script src="../js/logout.js"></script>
 
 </body>
 
